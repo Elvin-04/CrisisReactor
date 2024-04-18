afbeb59955222a6ab47b6bf19b7d63b6a3e127fe
Lucas Blanc <118185400+LucasBlancLCS@users.noreply.github.com>
2024-04-18 11:40:49 +0200

    coding digicode

---
+++ CrisisReactor/Assets/Scripts/Digicode/DC_DigicodeCodesList.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DC_DigicodeCodesList
+{
+    public static List<string> correctCodes = new List<string> { "1111", "2222", "3333", "4444", "5555", "6666", "7777", "8888", "9999", "1000"};
+}
---
+++ CrisisReactor/Assets/Scripts/Digicode/DC_GameManager.cs
@@ -0,0 +1,65 @@
+using TMPro;
+using UnityEngine;
+
+public class DC_GameManager : MonoBehaviour
+{
+    private string enteredCode;
+    private string waitedCode;
+    [SerializeField] private TextMeshProUGUI enteredCodeText;
+
+    void Awake()
+    {
+        InitCodeFromPool();
+    }
+
+    private void InitCodeFromPool()
+    {
+        int randomizedIndex = Random.Range(0, DC_DigicodeCodesList.correctCodes.Count - 1);
+        waitedCode = DC_DigicodeCodesList.correctCodes[randomizedIndex];
+        Debug.Log("good code = " + waitedCode);
+    }
+
+    public void enterNumber(int _newNumber)
+    {
+        if(enteredCode != null)
+        {
+            if(enteredCode.Length < 4)
+            {
+                enteredCode += _newNumber;
+                UpadteEnteredTextUI();
+            }
+        }
+        else
+        {
+            enteredCode += _newNumber;
+            UpadteEnteredTextUI();
+        }
+
+    }
+
+    private void CheckCode()
+    {
+        if(enteredCode == waitedCode)
+        {
+            Debug.Log("winned");
+            DC_DigicodeCodesList.correctCodes.Remove(enteredCode);
+        }
+        else
+        {
+            Debug.Log("wrong code");
+            enteredCode = "";
+            UpadteEnteredTextUI();
+            InitCodeFromPool();
+        }
+    }
+
+    public void ValidCode()
+    {
+        CheckCode();
+    }
+    
+    private void UpadteEnteredTextUI()
+    {
+        enteredCodeText.text = enteredCode;
+    }
+}
