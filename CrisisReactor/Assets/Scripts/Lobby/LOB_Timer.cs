9efddc9a55b725446dc066e2c96eb7d601cf6115
Elvin-04 <e.berger@lahorde.school>
2024-04-19 09:56:53 +0100

    Comment + loose condition

--- CrisisReactor/Assets/Scripts/Lobby/LOB_Timer.cs
+++ CrisisReactor/Assets/Scripts/Lobby/LOB_Timer.cs
@@ -1,9 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class LOB_Timer : MonoBehaviour
 {
@@ -12,7 +8,10 @@ public class LOB_Timer : MonoBehaviour
     [SerializeField] private float currentTime = 300;
     [SerializeField] private TextMeshProUGUI timerText;
     [HideInInspector] public int value = 0;
+    int currentTimeInt;
 
+
+    //Create DontDestroyOnLoad scene 
     private void Start()
     {
         GameObject[] multiScene = GameObject.FindGameObjectsWithTag("MultiSceneManager");
@@ -40,12 +39,29 @@ public class LOB_Timer : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //Update the timer variable
         currentTime -= Time.unscaledDeltaTime;
+        //Update and set the time text on screen
         if (timerText != null)
             timerText.text = ((int)currentTime / 60).ToString("00") + ":" + ((int)currentTime % 60).ToString("00");
         else
             if(GameObject.Find("TimerText") != null)
                 timerText = GameObject.Find("TimerText").GetComponent<TextMeshProUGUI>();
+
+        //Play a tick sound each second
+        if(currentTimeInt != (int)currentTime)
+        {
+            currentTimeInt = (int)currentTime;
+            GetComponent<AudioSource>().Play();
+        }
+
+        //Loose condition
+        if(currentTime <= 0)
+        {
+            Debug.Log("Loose");
+            //Play anim or sounds here
+        }
+
     }
 
 }
