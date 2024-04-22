35e1c465055e91587f99c49720b234ab52e0e685
lucasdprx <l.darpeix@lahorde.school>
2024-04-22 22:01:10 +0200

    Move with button

--- CrisisReactor/Assets/Scripts/Onde/O_MoveOnde.cs
+++ CrisisReactor/Assets/Scripts/Onde/O_MoveOnde.cs
@@ -1,14 +1,19 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class O_MoveOnde : MonoBehaviour
 {
     [SerializeField] private List<Image> onde = new();
     [SerializeField] private GameObject ondes;
     [SerializeField] private float speed;
     private int indexMove;
-    private int timer;
+    private float timer;
+    private bool canMove;
+    [SerializeField] private float second;
+
+    [SerializeField] private GameObject button1;
+    [SerializeField] private GameObject button2;
 
     private void Start()
     {
@@ -16,31 +21,56 @@ public class O_MoveOnde : MonoBehaviour
     }
     void FixedUpdate()
     {
-        //Move();
+        if (canMove)
+        {
+            Move();
+        }
     }
 
     private void Move()
     {
-        ondes.transform.position += new Vector3(speed * Time.fixedDeltaTime, 0, 0);
-        timer++;
-        if (timer >= 100)
+        for (int i = 0; i < onde.Count; i++)
+        {
+            onde[i].transform.position += new Vector3(speed * Time.fixedDeltaTime, 0, 0);
+        }
+        timer += Time.fixedDeltaTime;
+        if (timer >= 0.99f * ondes.transform.localScale.x)
         {
             timer = 0;
             if (indexMove >= onde.Count - 1)
             {
                 onde[onde.Count - 1].transform.position = onde[0].transform.position;
-                onde[onde.Count - 1].transform.position -= new Vector3(200, 0, 0);
+                onde[onde.Count - 1].transform.position -= new Vector3(100 * ondes.transform.localScale.x, 0, 0);
                 indexMove--;    
             }
             else
             {
                 onde[indexMove].transform.position = onde[indexMove + 1].transform.position;
-                onde[indexMove].transform.position -= new Vector3(200, 0, 0);
+                onde[indexMove].transform.position -= new Vector3(100 * ondes.transform.localScale.x, 0, 0);
                 if (indexMove <= 0)
                     indexMove = onde.Count - 1;
                 else
                     indexMove--;
             }
         }
+    }
+
+    public void StartMove()
+    {
+        if (!canMove)
+        {
+            canMove = true;
+            button1.SetActive(false);
+            button2.SetActive(false);
+            StartCoroutine(StopMoving(second));
+        }
+    }
+
+    IEnumerator StopMoving(float second)
+    {
+        yield return new WaitForSeconds(second);
+        canMove = false;
+        button1.SetActive(true);
+        button2.SetActive(true);
     }
 }
